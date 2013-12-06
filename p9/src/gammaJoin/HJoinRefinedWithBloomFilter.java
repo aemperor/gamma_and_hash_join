d405c6024d4ea9d8d01982e2a5d15e8d6a094a26
Alexis <aemperador83@yahoo.com>
2013-12-06 00:36:14 -0600

    HJoinRefinedWithBloomFilter to support Gamma.ArrayConnectors

--- p9/src/gammaJoin/HJoinRefinedWithBloomFilter.java
+++ p9/src/gammaJoin/HJoinRefinedWithBloomFilter.java
@@ -11,23 +11,20 @@ public class HJoinRefinedWithBloomFilter extends ArrayConnectors {
 	public void join(String input1, String input2, int jk1, int jk2) {
 		ThreadList.init();
 
-		Connector[] inputs = ArrayConnectors.initConnectorArray("inputs");
-		
-		inputs[0] = new Connector(input1);
-		inputs[1] = new Connector(input2);
-		
-		ReadRelation r1 = new ReadRelation("client.txt", inputs[0]);
-		ReadRelation r2 = new ReadRelation("view.txt", inputs[1]);
+		Connector c1 = new Connector(input1);
+		ReadRelation r1 = new ReadRelation("client.txt", c1);
+		Connector c2 = new Connector(input2);
+		ReadRelation r2 = new ReadRelation("view.txt", c2);
 
-		inputs[2] = new Connector("Bloom->BFilter");
-		inputs[3]  = new Connector("Bloom->HJoin");
-		Bloom b = new Bloom(inputs[0], inputs[1], inputs[2], jk1);
+		Connector c3 = new Connector("Bloom->BFilter");
+		Connector c4 = new Connector("Bloom->HJoin");
+		Bloom b = new Bloom(c1, c4, c3, jk1);
 
-		inputs[4] = new Connector("BFilter->HJoin");
-		BFilter filter = new BFilter(inputs[1], inputs[2], inputs[4], jk2);
+		Connector c5 = new Connector("BFilter->HJoin");
+		BFilter filter = new BFilter(c2, c3, c5, jk2);
 
 		Connector dataOutput =  new Connector("output");
-		HJoin dataJoin = new HJoin(inputs[3], inputs[4], jk1, jk2, dataOutput);
+		HJoin dataJoin = new HJoin(c4, c5, jk1, jk2, dataOutput);
 		Print p = new Print(dataOutput);
 
 		try{
