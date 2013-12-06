a7e07700329b7c6979b27f088ca3a05a0f78b3a4
Alexis <aemperador83@yahoo.com>
2013-12-05 23:56:31 -0600

    MapReducedBFilter to support Gamma.ArrayConnectors

--- p9/src/gammaJoin/MapReducedBFilter.java
+++ p9/src/gammaJoin/MapReducedBFilter.java
@@ -11,19 +11,22 @@ public class MapReducedBFilter extends ArrayConnectors {
 	public void join(String input1, String input2, int jk1, int jk2) {
 		ThreadList.init();
 		
-		Connector dataIn1 = new Connector("Reader->HSplit1");
-		Connector dataIn2 = new Connector("Reader->HSplit2");
+		Connector[] inputs = ArrayConnectors.initConnectorArray("dataIn");
+		inputs[0] = new Connector("Reader->HSplit1");
+		inputs[1] = new Connector("Reader->HSplit2");
 		
-		ReadRelation reader1 = new ReadRelation(input1, dataIn1);
-		ReadRelation reader2 = new ReadRelation(input2, dataIn2);
+		
+		
+		ReadRelation reader1 = new ReadRelation(input1, inputs[0]);
+		ReadRelation reader2 = new ReadRelation(input2, inputs[1]);
 		
 		Connector bloomHJoin = new Connector("BloomData->HJoin");
 		Connector bloomBitMap = new Connector("BloomBitMap->BFilter");
-		Bloom bloom = new Bloom(dataIn1, bloomHJoin, bloomBitMap, jk1);
+		Bloom bloom = new Bloom(inputs[0], bloomHJoin, bloomBitMap, jk1);
 		
-		Connector [] dataSplitedIn = new Connector[GammaConstants.splitLen]; 
-		Connector [] bitMapSplitIn = new Connector[GammaConstants.splitLen];
-		Connector [] dataFilteredOut = new Connector[GammaConstants.splitLen];
+		Connector [] dataSplitedIn = ArrayConnectors.initConnectorArray("dataSplitedIn"); 
+		Connector [] bitMapSplitIn = ArrayConnectors.initConnectorArray("bitMapSplitIn");
+		Connector [] dataFilteredOut = ArrayConnectors.initConnectorArray("dataFilteredOut");
 		
 		for (int i = 0; i < GammaConstants.splitLen; i++) {
 			dataSplitedIn[i] = new Connector("HSplit->BFilter_2_"+i);
@@ -31,7 +34,7 @@ public class MapReducedBFilter extends ArrayConnectors {
 			dataFilteredOut[i] = new Connector("BFilter->Merge_"+i);
 		}
 		
-		HSplit dataSplit = new HSplit(dataIn2, jk2, dataSplitedIn);
+		HSplit dataSplit = new HSplit(inputs[1], jk2, dataSplitedIn);
 		SplitM mapSlit = new SplitM(bloomBitMap, bitMapSplitIn);
 		
 		BFilter [] filterSet = new BFilter[GammaConstants.splitLen];
